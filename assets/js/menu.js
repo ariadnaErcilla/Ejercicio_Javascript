334a2841e3d5d088729e66705e7fa914ec5e523a
Ariadna Ercilla <a.ercilla.m@gmail.com>
2015-05-19 16:37:18 +0200

    cambios JAvascript

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,33 +1,32 @@
 $(document).ready(function(){
 
-    //Javascript para cambiar la classe 'current-menu-item' de li
-    $('.clearfix li').click(function (e){
-        e.preventDefault();
+    var  liLista = $('.clearfix li');
+    var contenidoMenu = $('#menu-container');
+    var cruzMobil = $('#mobile-close');
+    var botonMobil = $('#mobile-button');
 
+    //Javascript para cambiar la classe 'current-menu-item' de li
+    liLista.click(function (e){
+        e.preventDefault();
         if($(this).hasClass('current-menu-item')){
             $(this).removeClass('current-menu-item');
         }
-
         else{
-            $('.clearfix li').removeClass('current-menu-item');
+            liLista.removeClass('current-menu-item');
             $(this).addClass('current-menu-item');
         }
     });
 
     // Hover para los elementos
-    $(".clearfix li").hover(function() {
-        $(this).children("ul").fadeIn('fast');
+    liLista.hover(function() {
+        $(this).children('ul').fadeIn('fast');
         },
         function(){
-        $(this).children("ul").fadeOut('fast');
+        $(this).children('ul').fadeOut('fast');
         });
 
 
     // Cambiar icono hamburguesa a cruz y mostrar menu mobil
-    var contenidoMenu = $('#menu-container');
-    var cruzMobil = $('#mobile-close');
-    var botonMobil = $('#mobile-button');
-
     botonMobil.click(function() {
         contenidoMenu.show();
         cruzMobil.show();
