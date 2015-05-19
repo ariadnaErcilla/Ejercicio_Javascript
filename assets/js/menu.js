bfb47ad71389bf0b7dae8166af9c12672dde95da
Ariadna Ercilla <a.ercilla.m@gmail.com>
2015-05-19 16:28:14 +0200

    entrega Javascript

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,67 +1,43 @@
 $(document).ready(function(){
-    //Javascript para marcar el elemento del menú seleccionado
-    var esteElemento = $(this);
 
-    $('.clearfix li').click(function(evento){
-        evento.preventDefault();
+    //Javascript para cambiar la classe 'current-menu-item' de li
+    $('.clearfix li').click(function (e){
+        e.preventDefault();
 
-        if(esteElemento.hasClass('current-menu-item')){
-            esteElemento.removeClass('current-menu-item');
+        if($(this).hasClass('current-menu-item')){
+            $(this).removeClass('current-menu-item');
         }
 
         else{
             $('.clearfix li').removeClass('current-menu-item');
             $(this).addClass('current-menu-item');
-            /*si elimino $(this) y pongo variable 'esteElemento' ya no funciona la función.
-            Sólo puedo usar la variable donde la he cambiado, en los otros sitios me peta sin sentido.
-            Yo había puesto:
-                else{
-                $('.clearfix li').removeClass('current-menu-item');
-                esteElemento.addClass('current-menu-item');
-                }  */
         }
     });
-});
+
+    // Hover para los elementos
+    $(".clearfix li").hover(function() {
+        $(this).children("ul").fadeIn('fast');
+        },
+        function(){
+        $(this).children("ul").fadeOut('fast');
+        });
 
 
-//Javascript para el menú mobil
-$(document).ready(function(){
+    // Cambiar icono hamburguesa a cruz y mostrar menu mobil
     var contenidoMenu = $('#menu-container');
     var cruzMobil = $('#mobile-close');
     var botonMobil = $('#mobile-button');
 
-        botonMobil.click(function() {
-            contenidoMenu.show();
-            cruzMobil.show();
-            botonMobil.hide();
-         });
-
-        $('#mobile-close').click(function() {
-            contenidoMenu.hide();
-            cruzMobil.hide();
-            botonMobil.show();
-        });
+    botonMobil.click(function() {
+        contenidoMenu.show();
+        cruzMobil.show();
+        botonMobil.hide();
     });
 
+    cruzMobil.click(function() {
+        contenidoMenu.hide();
+        cruzMobil.hide();
+        botonMobil.show();
+    });
 
-
-/*
-    $('clearfix li').hover(function (evento) {
-       $(this).fadeToggle('2000,swing');
-   });
-
-
-
-    $('clearfix li').hover(function () {
-  if ($('li.current-menu-item').is(':visible')) {
-    $('li').fadeOut('fast');
-  } else {
-    $('li').fadeIn('slow');
-  };
 });
-*/
-
-
-
-
-
